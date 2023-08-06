f2e5d1ea6a25210e17e6f576edef176567d1e522
minsang-alt <mazinger97@naver.com>
2023-08-06 21:54:09 +0900

    Feat: '기본레포지토리 구성'

---
+++ src/test/java/com/BEACON/beacon/BeaconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BEACON.beacon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeaconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
