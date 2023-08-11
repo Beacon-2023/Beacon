85e29f404addfcf59551fb088acfca541bfbcf86
minsang-alt <mazinger97@naver.com>
2023-08-11 17:53:24 +0900

    Refactor: MemberDto 코드 리펙토링

--- src/main/java/com/BEACON/beacon/member/dto/MemberDto.java
+++ src/main/java/com/BEACON/beacon/member/dto/MemberDto.java
@@ -5,6 +5,7 @@ import com.BEACON.beacon.member.domain.MemberEntity;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Getter @ToString
 public class MemberDto {
 
+    @NotEmpty
     private String userId;
 
     @NotEmpty
@@ -24,9 +26,19 @@ public class MemberDto {
             regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#!~$%^&-+=()])(?=\\S+$).{8,16}$")
     private String password;
 
+    private MemberDto(){
+    }
+
+    @Builder
+    public MemberDto(String userId, String email, String password) {
+        this.userId = userId;
+        this.email = email;
+        this.password = password;
+    }
 
     public static MemberEntity toEntity(MemberDto memberDto, PasswordEncoder passwordEncoder) {
         return MemberEntity.builder()
+                .userId(memberDto.getUserId())
                 .email(memberDto.getEmail())
                 .status(MemberStatus.ACTIVE)
                 .password(passwordEncoder.encode(memberDto.getPassword()))
