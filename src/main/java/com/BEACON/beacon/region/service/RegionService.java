bf9ad38f06eea16d01c0942767de2129972252e0
enble <enble_777@naver.com>
2023-08-17 23:04:42 +0900

    Feat: RegionService 추가
    RegionService 추가했습니다.

---
+++ src/main/java/com/BEACON/beacon/region/service/RegionService.java
@@ -0,0 +1,26 @@
+package com.BEACON.beacon.region.service;
+
+import com.BEACON.beacon.region.domain.Region;
+import com.BEACON.beacon.region.dto.RegionDto;
+import com.BEACON.beacon.region.dto.RegionMapper;
+import com.BEACON.beacon.region.exception.NoSuchRegionException;
+import com.BEACON.beacon.region.repository.RegionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class RegionService {
+
+    private final RegionRepository repository;
+
+    public RegionDto findRegionByCode(String code) {
+        Region region = repository.findRegionByCode(code).orElseThrow(
+                () -> new NoSuchRegionException("코드에 맞는 지역이 존재하지 않습니다.")
+        );
+        return RegionMapper.toDto(region);
+    }
+
+}
