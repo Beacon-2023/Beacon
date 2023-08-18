29613a0cc2edeca703de9014870d99ec804056dc
enble <enble_777@naver.com>
2023-08-18 15:05:02 +0900

    Feat: RegionAlert의 연관관계 편의 메서드 추가
    연관관계 편의 메서드가 추가되었습니다.

--- src/main/java/com/BEACON/beacon/region/domain/RegionAlert.java
+++ src/main/java/com/BEACON/beacon/region/domain/RegionAlert.java
@@ -1,6 +1,10 @@
 package com.BEACON.beacon.region.domain;
 
+import com.BEACON.beacon.region.dto.RegionDto;
+import com.BEACON.beacon.region.dto.RegionMapper;
 import com.BEACON.beacon.scraping.domain.DisasterAlert;
+import com.BEACON.beacon.scraping.dto.DisasterAlertDto;
+import com.BEACON.beacon.scraping.dto.DisasterAlertMapper;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -17,7 +21,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegionAlert {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "REGION_ALERT_ID")
     private Long id;
 
@@ -33,5 +38,21 @@ public class RegionAlert {
         this.id = id;
         this.region = region;
         this.disasterAlert = disasterAlert;
+    }
+
+    public void setRegion(RegionDto dto) {
+        this.region = RegionMapper.toEntity(dto);
+    }
+
+    public void setDisasterAlert(DisasterAlertDto dto) {
+        if (this.disasterAlert != null) {
+            this.disasterAlert.getRegionAlertList().remove(this);
+        }
+
+        DisasterAlert entity = DisasterAlertMapper.toEntity(dto);
+        this.disasterAlert = entity;
+        if (!entity.getRegionAlertList().contains(this)) {
+            entity.getRegionAlertList().add(this);
+        }
     }
 }
