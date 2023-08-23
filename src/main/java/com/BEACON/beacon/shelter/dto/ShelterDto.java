d54e3c3e04e67535b0bf5ad0132899550db15e12
enble <enble_777@naver.com>
2023-08-23 18:45:38 +0900

    Feat: ShelterDto 추가
    ShelterDto 추가

---
+++ src/main/java/com/BEACON/beacon/shelter/dto/ShelterDto.java
@@ -0,0 +1,41 @@
+package com.BEACON.beacon.shelter.dto;
+
+import com.BEACON.beacon.region.domain.Region;
+import com.BEACON.beacon.shelter.domain.ShelterCategory;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+public class ShelterDto {
+    private Long id;
+
+    private ShelterCategory shelterCategory;
+
+    private String name;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    private String tel;
+
+    private Region region;
+
+    public ShelterDto(Long id, ShelterCategory shelterCategory, String name, Double latitude,
+            Double longitude, String tel, Region region) {
+        this.id = id;
+        this.shelterCategory = shelterCategory;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.tel = tel;
+        this.region = region;
+    }
+}
