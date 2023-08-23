4cfbb46c4d1e4d900d49f648b0eedcfff66c3662
minsang-alt <mazinger97@naver.com>
2023-08-24 00:31:03 +0900

    Refactor: 404 status 추가

--- src/main/java/com/BEACON/beacon/global/HttpStatusResponse.java
+++ src/main/java/com/BEACON/beacon/global/HttpStatusResponse.java
@@ -5,13 +5,15 @@ import org.springframework.http.ResponseEntity;
 
 public interface HttpStatusResponse {
     //200
-     ResponseEntity<HttpStatus> RESPONSE_OK = ResponseEntity.status(HttpStatus.OK).build();
+    ResponseEntity<HttpStatus> RESPONSE_OK = ResponseEntity.status(HttpStatus.OK).build();
     // 201
-     ResponseEntity<HttpStatus> RESPONSE_CREATED = ResponseEntity.status(HttpStatus.CREATED).build();
+    ResponseEntity<HttpStatus> RESPONSE_CREATED = ResponseEntity.status(HttpStatus.CREATED).build();
     //403
-     ResponseEntity<HttpStatus> RESPONSE_FORBIDDEN = ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    ResponseEntity<HttpStatus> RESPONSE_FORBIDDEN = ResponseEntity.status(HttpStatus.FORBIDDEN).build();
     //409
-     ResponseEntity<HttpStatus> RESPONSE_CONFLICT = ResponseEntity.status(HttpStatus.CONFLICT).build();
+    ResponseEntity<HttpStatus> RESPONSE_CONFLICT = ResponseEntity.status(HttpStatus.CONFLICT).build();
     //400
-     ResponseEntity<HttpStatus> RESPONSE_BAD_REQUEST = ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    ResponseEntity<HttpStatus> RESPONSE_BAD_REQUEST = ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    //404
+    ResponseEntity<HttpStatus> RESPONSE_NOTFOUND = ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 }
