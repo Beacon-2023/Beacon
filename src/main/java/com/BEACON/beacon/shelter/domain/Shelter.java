89ba19a725ab75ed50818469638740c966fcb657
enble <enble_777@naver.com>
2023-08-23 18:44:53 +0900

    Feat: Shelter 엔티티 추가
    대피장소를 위한 Shelter 엔티티를 추가하였습니다.

---
+++ src/main/java/com/BEACON/beacon/shelter/domain/Shelter.java
@@ -0,0 +1,50 @@
+package com.BEACON.beacon.shelter.domain;
+
+import com.BEACON.beacon.region.domain.Region;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+public class Shelter {
+
+    @Id
+    @Column(name = "SHELTER_ID")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private ShelterCategory shelterCategory;
+
+    private String name;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    private String tel;
+
+    @ManyToOne()
+    @JoinColumn(name = "REGION_CODE")
+    private Region region;
+
+    public Shelter(Long id, ShelterCategory shelterCategory, String name, Double latitude,
+            Double longitude, String tel, Region region) {
+        this.id = id;
+        this.shelterCategory = shelterCategory;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.tel = tel;
+        this.region = region;
+    }
+}
