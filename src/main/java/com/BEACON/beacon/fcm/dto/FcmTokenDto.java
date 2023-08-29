2be04f0e068a6949560b82bf1ee79fb33aed4268
minsang-alt <mazinger97@naver.com>
2023-08-29 22:38:15 +0900

    Docs: Swagger 문서 정의

--- src/main/java/com/BEACON/beacon/fcm/dto/FcmTokenDto.java
+++ src/main/java/com/BEACON/beacon/fcm/dto/FcmTokenDto.java
@@ -1,16 +1,19 @@
 package com.BEACON.beacon.fcm.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 
 @Getter
+@Schema(description = "FCM 토큰 정보 전달 객체")
 public class FcmTokenDto {
     //토큰 , 회원 아이디
     @NotEmpty
+    @Schema(description = "FCM 토큰")
     private String token;
 
+    @Schema(description = "회원의 아이디", example = "beacon1234")
     private String userName;
-
 
 
 }
