712dea73cb653b80d37f853afed4b39e6f603e08
minsang-alt <mazinger97@naver.com>
2023-09-05 00:04:43 +0900

    Feat: 커스텀가이드라인 서비스 인터페이스

--- src/main/java/com/BEACON/beacon/guideline/service/GuideLineService.java
+++ src/main/java/com/BEACON/beacon/guideline/service/GuideLineService.java
@@ -1,8 +1,21 @@
 package com.BEACON.beacon.guideline.service;
 
+import com.BEACON.beacon.guideline.dto.CustomGuideLineDto;
 import com.BEACON.beacon.guideline.response.BasicGuideLineResponse;
+import com.BEACON.beacon.guideline.response.CustomGuideLineResponse;
 import com.BEACON.beacon.scraping.domain.DisasterCategory;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.List;
 
 public interface GuideLineService {
     public BasicGuideLineResponse getBasicGuideLine(DisasterCategory disasterCategory);
+
+    public void registerCustomGuideLine(CustomGuideLineDto customGuideLineDto, HttpServletRequest request);
+
+    public List<CustomGuideLineResponse> getAllCustomGuideLine(HttpServletRequest request);
+
+    public CustomGuideLineResponse getCustomGuideLine(HttpServletRequest request,DisasterCategory disasterCategory);
+
+    public void updateCustomGuideLine(HttpServletRequest request,CustomGuideLineDto customGuideLineDto);
 }
