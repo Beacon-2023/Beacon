9212ebc3d9901c19ec474fba34494d253c43042c
minsang-alt <mazinger97@naver.com>
2023-09-05 00:04:16 +0900

    Feat: 커스텀가이드라인 레포지토리 및 응답객체 구현

--- src/main/java/com/BEACON/beacon/guideline/response/BasicGuideLineResponse.java
+++ src/main/java/com/BEACON/beacon/guideline/response/BasicGuideLineResponse.java
@@ -1,5 +1,6 @@
 package com.BEACON.beacon.guideline.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Schema(description = "커스텀 가이드라인 응답 객체")
 public class BasicGuideLineResponse {
     private String title;
     private String content;
